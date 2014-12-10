9b05b2444e58ee8b63042573469758ab395778dd
root <root@host.host>
2014-12-10 15:24:02 -0600

    Implement the beginnings of anchored drip-sort

--- sort.py
+++ sort.py
@@ -1,28 +1,93 @@
 import numpy
 import cv2
 
-def sort_by_row(image):
-    return numpy.sort(image, axis=1)
+image_to_edit = 'test_line.png'
+save_name = 'test.png'
+black_value = 150;
 
-def invert(image, file_name):
-    image = (255-image)
-    cv2.imwrite(file_name, image)
+img = cv2.imread(image_to_edit)
+height = img.shape[0]
+width = img.shape[1]
+row = 0
+column = 0
+unsorted_pixels = numpy.array([])
 
-def sort_by_column(image):
-    return numpy.sort(image, axis=0)
+def sortByRow(img):
+    return numpy.sort(img, axis=1)
 
-def get_luminance(pixel):
+def invert(img, file_name):
+    img = (255-img)
+    return img
+
+def sortByColumn(img):
+    return numpy.sort(img, axis=0)
+
+def getLuminance(pixel):
     R = pixel[2]
     G = pixel[1]
     B = pixel[0]
     return (0.2126*R + 0.7152*G + 0.0722*B)
 
-image = cv2.imread('image12.png')
+def blotDarkPixels(img):
+    for row in range(0, img.shape[0]):
+        for column in range(0, img.shape[1]):
+            pixel = img[row][column]
+            if get_luminance(pixel) < 150:
+                img[row][column] = [0, 0, 0]
+    return img
 
-for row in range(0, image.shape[0]):
-    for column in range(0, image.shape[1]):
-        pixel = image[row][column]
-        if get_luminance(pixel) < 150:
-            image[row][column] = [0, 0, 0]
+def sortColumn():
+    x = column
+    y = 0
+    y_end = 0
 
-cv2.imwrite('test.png', image)
+    while y_end < height-1:
+        y = getFirstNonBlackY(x, y)
+        y_end = getNextBlackY(x,y)
+
+        if (y < 0):
+            break
+
+        sort_length = y_end - y
+        
+        for i in range (0, sort_length):
+            unsorted_pixels = numpy.concatenate((unsorted_pixels, img[y+i][x]))
+
+        unsorted_pixels.sort()
+        sorted_pixels = unsorted_pixels
+        print sorted_pixels
+
+        for i in range(0, sort_length):
+            # POTENTIALLY WRONG
+            img[y+i][x] = sorted_pixels[i]
+
+        y = y_end + 1
+
+
+
+def getFirstNonBlackY(_x, _y):
+    x = _x
+    y = _y
+    if y < height:
+        while (getLuminance(img[y][x]) < black_value):
+            y += 1
+            if y >= height:
+                return -1
+    return y
+
+def getNextBlackY(_x, _y):
+    x = _x
+    y = _y + 1
+    if y < height:
+        while (getLuminance(img[y][x]) > black_value):
+            y += 1
+            if y >= height:
+                return height-1
+    return y-1
+
+
+while column <= width-1:
+    sortColumn()
+    column += 1
+
+cv2.imwrite(save_name, img)
